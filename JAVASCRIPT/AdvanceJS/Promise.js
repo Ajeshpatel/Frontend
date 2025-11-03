a865e78f143bb3a8068d2c5f37c4b4956378df27
Ajesh patel <ajeshpatel10@gmail.com>
2025-11-03 16:12:18 +0530

    promise using setTimeout

--- JAVASCRIPT/AdvanceJS/Promise.js
+++ JAVASCRIPT/AdvanceJS/Promise.js
@@ -1,7 +1,38 @@
 // Promise In JS
 
+// p = new Promise((resolve, reject) => {
+//     app = "BMS";
+
+//     if (app === "BMS"){
+//         resolve("1. Open BMS");
+//     }else {
+//         reject("Please Consider BMS not fount");
+        
+//     }
+// })
+// p.then((msg) => {
+//     console.log(msg)
+//     console.log("2. search movie");
+// })
+// .then(() => {
+//     console.log("3. select seat");
+// })
+// .then(() => {
+//     console.log("4. Ticket Booked");
+// })
+// .catch((e) => {
+//     console.log(e);
+// })
+// .finally(() => {
+//     console.log("Close BMS app");
+// });
+
+
+// try to write inside setTimeOut function
+
 p = new Promise((resolve, reject) => {
-    app = "BMS";
+    setTimeout(() => {
+            app = "BS";
 
     if (app === "BMS"){
         resolve("1. Open BMS");
@@ -9,23 +40,47 @@ p = new Promise((resolve, reject) => {
         reject("Please Consider BMS not fount");
         
     }
-})
-p.then((msg) => {
-    console.log(msg)
-    console.log("2. search movie");
-})
-.then(() => {
-    console.log("3. select seat");
-})
-.then(() => {
-    console.log("4. Ticket Booked");
-})
-.catch((e) => {
-    console.log(e);
-})
-.finally(() => {
-    console.log("Close BMS app");
+
+    },3000)
 });
 
-
-// try to write inside setTimeOut function
+p.then((app) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`2. Opened ${app}`);
+            resolve();
+        }, 2000);
+    });
+})
+.then(() => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("3. Search for a movie");
+            resolve();
+        }, 4000);
+    });
+})
+.then(() => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("4. Select seat");
+            resolve();
+        }, 8000);
+    });
+})
+.then(() => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("5. Ticket booked");
+            resolve();
+        }, 1000);
+    });
+})
+.catch((e) => {
+    console.log("Error Msg: ", e);
+})
+.finally(() => {
+    setTimeout(() => {
+        console.log("Closed phone");
+    }, 6000)
+});
